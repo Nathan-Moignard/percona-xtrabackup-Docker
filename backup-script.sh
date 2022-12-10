b6916688e8133d966f26c965643da42a210be8b8
Nathan.M <nathanmoignard@gmail.com>
2022-12-10 15:46:31 +0100

    [UPDATE] Replacing hard-coded variable to dynamic ones

--- backup-script.sh
+++ backup-script.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run --network=host -v "/home/nathan/Desktop/Mysql-Module/wait-for-it.sh:/wait-for-it.sh" -v "/home/nathan/Desktop/Mysql-Module/mysql:/var/lib/mysql" -v "/home/nathan/Desktop/Mysql-Module/backups/$(date +%H_%M_%S_%d-%m-%Y):/xtrabackup_backupfiles" perconalab/percona-xtrabackup:2.4 ./wait-for-it.sh 127.0.0.1:3306 -- xtrabackup --backup --host=127.0.0.1 --user=root --password=toor
+docker run --network=host -v "$(pwd)/wait-for-it.sh:/wait-for-it.sh" -v "$(pwd)/mysql:/var/lib/mysql" -v "$(pwd)/backups/$(date +%H_%M_%S_%d-%m-%Y):/xtrabackup_backupfiles" perconalab/percona-xtrabackup:2.4 ./wait-for-it.sh 127.0.0.1:3306 -- xtrabackup --backup --host=127.0.0.1 --user=root --password=toor
